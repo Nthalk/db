d5ccbd8dd18d1769902db0c88f27b3b8311addeb
Carl Taylor <carl@etaylor.me>
2018-08-23 22:43:12 -0700

    Fix docs

--- example/src/main/java/com/iodesystems/db/example/Application.java
+++ example/src/main/java/com/iodesystems/db/example/Application.java
@@ -1,20 +1,13 @@
 package com.iodesystems.db.example;
 
-import static org.slf4j.LoggerFactory.getLogger;
-
-import org.slf4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
 
-  private static final Logger LOG = getLogger(Application.class);
-
   public static void main(
-      String[] args) throws Exception {
+      String[] args) {
     SpringApplication.run(Application.class, args);
-
   }
-
 }
